987ec10c3f2e057da00e022fe0710778e720427b
Fineas <finymelly@gmail.com>
2024-11-27 15:31:44 +0200

    remove duplicates

--- transaction.py
+++
@@ -1,8 +0,0 @@
-"""
-Transaction Class
-"""
-class Transaction:
-    def __init__(self, proposer_id, answer, signature):
-        self.proposer_id = proposer_id
-        self.answer = answer
-        self.signature = signature 
